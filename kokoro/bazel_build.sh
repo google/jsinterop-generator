e17d7f657174cc43019e6995aef0179a2957015b
dramaix <dramaix@google.com>
2018-12-10 16:22:57 -0800

    Use setup_build_environment.sh script for setup kokoro builds for all our opensource repo.
    PiperOrigin-RevId: 224904609

--- kokoro/bazel_build.sh
+++ kokoro/bazel_build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+chmod +x ${KOKORO_GFILE_DIR}/setup_build_environment.sh
+${KOKORO_GFILE_DIR}/setup_build_environment.sh
+
 # the repo is cloned under git/jsinterop-base
 cd git/jsinterop-generator
 
