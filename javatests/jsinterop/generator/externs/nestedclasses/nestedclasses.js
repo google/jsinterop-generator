821e3351c9f81113ee3d4504636847c0ca259f87
Julien Dramaix <dramaix@google.com>
2019-10-15 12:35:02 -0700

    Add a test for nested class conversion.
    PiperOrigin-RevId: 274863800

---
+++ javatests/jsinterop/generator/externs/nestedclasses/nestedclasses.js
@@ -0,0 +1,46 @@
+// Copyright 2019 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Test nested classes conversion.
+ * @externs
+ */
+
+
+/** @constructor */
+function Foo() {}
+
+/** @return {string} */
+Foo.staticMethod = function() {};
+
+/** @constructor */
+Foo.Bar = function() {}
+
+          /** @type {string} */
+          Foo.Bar.staticStringField;
+
+/**
+ * @param {string} param
+ * @return {undefined}
+ */
+Foo.Bar.staticMethod = function(param) {}
+
+                       /** @type {string} */
+                       Foo.Bar.prototype.stringField;
+
+/**
+ * @param {string} param
+ * @return {undefined}
+ */
+Foo.Bar.prototype.method = function(param) {}
